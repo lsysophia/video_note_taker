54e023609cdd3e1f698fa1a1ca64a67bb2590b4c
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-11-05 14:30:45 -0600

    set up fetch

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -1,15 +1,41 @@
 import React from "react";
-import { render } from "react-dom";
+import fetchVideo from '../utility/fetchVideo.js'
 
-render() {
+const SearchVideo = (title) => {
+    [video, setVideo] = useState({})
+
+    function handleSearchSubmit(e) {
+        e.preventDefault()
+        fetchVideo.get('/search', {
+            params: {
+                q: e
+            }
+        })
+        setVideos(res)
+    }
+
     return (
         <div>
             <div className="search-box">
-                <form onSubmit={(e) => this.props.handleSearchSubmit(e)}>
+                <form onSubmit={(e) => this.handleSearchSubmit(e)}>
                     <input type="text" name="title" placeholder="Search by Title" onChange={this.props.handleInputChange} />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
         </div>
     )
+
 }
+
+// import React from "react";
+// import { MDBCol } from "mdbreact";
+
+// const SearchPage = () => {
+//   return (
+//     <MDBCol md="6">
+//       <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+//     </MDBCol>
+//   );
+// }
+
+export default SearchPage;
